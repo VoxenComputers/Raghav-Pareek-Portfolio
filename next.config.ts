b1462d6668f3735711134253354caffb35af028c
Raghav Pareek <raghuprk6@gmail.com>
2026-09-17 20:46:18 +0530

    Add custom headers for sequence routes

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,19 @@ const nextConfig: NextConfig = {
   compiler: {
     styledComponents: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/sequence/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
